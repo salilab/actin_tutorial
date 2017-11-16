dd50cbda4535d788611958e618631a6223a97878
Ben Webb <ben@salilab.org>
2017-11-16 10:11:02 -0800

    Add basic test infrastructure
    Pull in the tutorial_tools repository as a submodule,
    which lets us test this tutorial at every git push
    against both Python 2 and Python 3, for both the
    latest IMP nightly build and the most recent stable
    IMP release. Add a simple test of the modeling script.

---
+++ support/README.md
@@ -0,0 +1,3 @@
+This directory contains support infrastructure to make sure that this
+tutorial works correctly. It is of use only to IMP and tutorial developers,
+and does not need to be present in your own IMP applications.
---
+++ support/test/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import unittest
+import os
+import subprocess
+
+TOPDIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+class Tests(unittest.TestCase):
+    def test_modeling_script(self):
+        """Test the main modeling script"""
+        p = subprocess.check_call(["python", "modeling.py", "--test"],
+                                  cwd=os.path.join(TOPDIR, 'modeling'))
+        # todo: assert that it generated correct outputs
+
+
+if __name__ == '__main__':
+    unittest.main()
